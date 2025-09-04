dd3ee34844a8b8848befa6bc0c26e19c6f53a607
jhk <jhk06@mailbox.org>
2025-09-04 15:09:50 +0200

    feat(rofi): significantly improve pass applet

--- .config/rofi/applets/pass/pass.sh
+++ .config/rofi/applets/pass/pass.sh
@@ -23,24 +23,57 @@ type-string() {
     }
 }
 
+notify-on-copy() {
+    local seconds=${2:-45}
+    notify-send -i password "$1 copied to clipboard" \
+        "Clipboard will be cleared in ${seconds}s"
+}
+
+clear-clipboard() {
+    coproc {
+        local before="$(wl-paste | base64)"
+        sleep $1
+        local after="$(wl-paste | base64)"
+        if [[ "$before" == "$after" ]]; then
+            wl-copy -c
+        fi
+    }
+}
+
 perform-action() {
     local pass="$1"
     local action="$2"
     case "$action" in
     Copy)
         run pass -c "$pass"
+        notify-on-copy "Password"
         ;;
     Type)
         type-string "$(pass show "$pass" | head -n 1)"
         ;;
     "Copy OTP")
         run pass otp -c "$pass"
+        notify-on-copy "OTP Code"
         ;;
     "Type OTP")
         type-string "$(pass otp "$pass" | head -n 1)"
         ;;
     Show)
         pass show "$pass"
+        ;;
+    *)
+        read -r field action <<<"$action"
+        match="$(pass show "$pass" | awk -F: -v field="$field" 'tolower($1) == field { sub(/^[ \t]*/, "", $2); print $2; exit }')"
+        case "$action" in
+        typ*)
+            type-string "$match"
+            ;;
+        *)
+            run wl-copy "$match"
+            notify-on-copy "Field: '$field'"
+            clear-clipboard 45
+            ;;
+        esac
         ;;
     esac
 }
