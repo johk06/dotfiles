c4bef103a83434608051e9d9facea69420ffc39f
jhk <jhk06@mailbox.org>
2025-10-03 14:48:38 +0200

    nvim: update

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -1,9 +1,1 @@
 vim.cmd.compiler("gcc")
-
-vim.b.snippets = {
-    f = {
-        "${1:void} ${2:name}($3) {",
-        "    $0",
-        "}"
-    }
-}
--- .config/nvim/after/ftplugin/org.lua
+++
@@ -1,7 +0,0 @@
-vim.b.snippets = {
-    s = {
-        "#+begin_src ${1:lua}",
-        "$0",
-        "#+end_src"
-    }
-}
