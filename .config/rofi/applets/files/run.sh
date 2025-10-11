16368e302be0730b03a99a1abf1d91a40fcbc460
jhk <jhk06@mailbox.org>
2025-10-11 09:39:27 +0200

    rofi: move list styled theme for files out of that applet

--- .config/rofi/applets/files/run.sh
+++ .config/rofi/applets/files/run.sh
@@ -2,4 +2,4 @@
 
 exec rofi -show files\
     -modes "files:$XDG_CONFIG_HOME/rofi/applets/files/files.sh"\
-    -theme "$XDG_CONFIG_HOME/rofi/applets/files/theme.rasi"
+    -theme "$XDG_CONFIG_HOME/rofi/style/list-plain.rasi"
