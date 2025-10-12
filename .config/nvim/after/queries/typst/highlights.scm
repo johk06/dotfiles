aa6c8dce95da1ca08927b097dd22584021acb558
jhk <jhk06@mailbox.org>
2025-10-12 11:35:08 +0200

    nvim: improve typst symbol conceal
    Correctly handle .alt variants as well as a few extra symbols.
    This is not done however.

--- .config/nvim/after/queries/typst/highlights.scm
+++ .config/nvim/after/queries/typst/highlights.scm
@@ -1,6 +1,21 @@
 ; extends
 
-(formula (ident) @constant.typst
-    (#jhk-typst-set-symbol-conceal! @constant.typst))
-(formula (_ (ident) @constant.typst
+(formula
+  (ident) @constant.typst
+  (#jhk-typst-set-symbol-conceal! @constant.typst))
+
+(formula
+  (field
+    (ident)
+    field: (ident)) @constant.typst
+  (#jhk-typst-set-symbol-conceal! @constant.typst))
+
+(formula
+  (call
+    item: (ident) @function.call.typst
+    (#jhk-typst-set-symbol-conceal! @function.call.typst)))
+
+(formula
+  (attach
+    (ident) @constant.typst
     (#jhk-typst-set-symbol-conceal! @constant.typst)))
