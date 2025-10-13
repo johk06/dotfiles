42dd2ba864349f11331e63735dd7e730aa1e201d
jhk <jhk06@mailbox.org>
2025-10-13 19:49:40 +0200

    nvim: fix swapped <C-[.,]>, oops

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -712,8 +712,9 @@ map("i", "<C-f>", "<C-o>f", { remap = true })
 map("i", "<C-b>", "<C-o>F", { remap = true })
 -- quick way to trigger things like ]a in insert mode
 -- useful example: <C-,>a to go to the next argument
-map("i", "<C-,>", "<C-o>]", { remap = true })
-map("i", "<C-.>", "<C-o>[", { remap = true })
+map("i", "<C-.>", "<C-o>]", { remap = true })
+map("i", "<C-,>", "<C-o>[", { remap = true })
+
 
 -- leftover keys looking for a mapping
 -- <C-l>
