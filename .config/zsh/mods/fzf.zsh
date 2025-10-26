16bc0ea40d31d91d088ea107abca3692124706ee
jhk <jhk06@mailbox.org>
2025-10-26 09:22:21 +0100

    zsh: make better/more use of fzf

--- .config/zsh/mods/fzf.zsh
+++ .config/zsh/mods/fzf.zsh
@@ -3,64 +3,47 @@
 # wrappers around fzf for my most common use cases
 
 if [[ "$1" == "unload" ]]; then
-    unfunction fed fcd frg _fzf_shell_hist
+    unfunction cd/ _fzf_shell_hist _fzf_change_dir _fzf_insert_path
 
     unset FZF_DEFAULT_OPTS
     return
 fi
 
 FZF_DEFAULT_OPTS="--pointer='' --no-scrollbar --info=inline-right --no-separator --border=none --preview-border=line --height=~50% --no-bold"
-FZF_DEFAULT_OPTS+=" --color="\
-prompt:cyan,\
-fg:white,\
-bg:black,\
-bg+:gray,\
-gutter:black,\
-hl:yellow:underline,\
-hl+:yellow:underline,\
-info:magenta,\
-border:gray,\
-query:white:regular,\
-preview-fg:white,\
-preview-bg:black,\
-spinner:cyan,\
-marker:magenta,\
-header:white
+FZF_DEFAULT_OPTS+=" --color="prompt:cyan,fg:white,bg:black,bg+:gray,gutter:black,hl:yellow:underline,hl+:yellow:underline,info:magenta,border:gray,query:white:regular,preview-fg:white,preview-bg:black,spinner:cyan,marker:magenta,header:white
 
 export FZF_DEFAULT_OPTS
 
-# find & edit
-function fed {
-    local res="$(fd $@ --type=file|fzf --prompt="ed: " --preview='bat -p --color=always -- {}')"
-    if [[ -z "$res" ]]; then
-        return
-    fi
-    local parent="${res:h}"
-    local abs="${res:A}"
-    (cd "$parent"; "$EDITOR" "$abs")
-}
-compdef fed=fd
-
 # find * cd
-function fcd {
+function cd/ {
     local res="$(fd $@ --type=dir | fzf --prompt="cd: " --preview='lsd -l -- {}')"
     if [[ -n "$res" ]]; then
         cd "$res"
     fi
 }
-compdef fcd=fd
+compdef cd/=fd
 
 function _fzf_change_dir {
-    fcd
+    cd/
     zle reset-prompt
 }
 
 zle -N fzf-change-dir _fzf_change_dir
 bindkey "^[c" fzf-change-dir
 
+function _fzf_insert_path {
+    local res="$(fd $@ | fzf --prompt="file: " --preview='bat -p --color=always -- {}')"
+    if [[ -n "$res" ]]; then
+        LBUFFER+="${res:q}"
+        zle redisplay
+    fi
+}
+zle -N fzf-insert-path _fzf_insert_path
+bindkey "^[f" fzf-insert-path
+
 # search shell history
 function _fzf_shell_hist {
-    local res="$(fc -n -l $HISTSIZE|awk '!seen[$2]++'|fzf --no-sort --tac --prompt="hist: " -q "^$BUFFER")"
+    local res="$(fc -n -l $HISTSIZE | awk '!seen[$2]++' | fzf --no-sort --tac --prompt="hist: " -q "^$BUFFER")"
     if [[ -n "$res" ]]; then
         BUFFER="${res}"
         zle end-of-line
