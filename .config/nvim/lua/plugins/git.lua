914ca553997bda6432f16dcf2a567db7a48aefca
jhk <jhk06@mailbox.org>
2025-11-01 20:30:15 +0100

    nvim: more global git maps

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -100,6 +100,7 @@ local map_global_maps = function()
     map("n", "H", "<cmd>G! difftool .<cr>", { desc = "Git: Hunks to qflist" })
     map("n", "<C-h>", "<cmd>G difftool -y .<cr>", { desc = "Git: Diff all in tabs" })
     map("n", "<C-s>", "<cmd>Git add -p .<cr>", { desc = "Git: Stage interactively" })
+    map("n", "<C-r>", "<cmd>Git restore -p .<cr>", { desc = "Git: Reset interactively" })
 
     map("n", "g", function()
         git_relative_buf = vim.api.nvim_get_current_buf()
