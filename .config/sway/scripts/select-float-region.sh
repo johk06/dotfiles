54b2a4c5f76b189fcb93d5d8730c022e834a0e84
jhk <jhk06@mailbox.org>
2025-11-02 10:28:47 +0100

    sway: fix order of operations for floating resize

--- .config/sway/scripts/select-float-region.sh
+++ .config/sway/scripts/select-float-region.sh
@@ -2,5 +2,5 @@
 
 slurp -w 0 -b '#4c566acc' -s '#ffffff00' -f '%x %y %w %h' | {
     read -r x y w h
-    swaymsg floating enable';' move absolute position ${x}px ${y}px';' resize set width "$w" height "$h"
+    swaymsg floating enable';' resize set width "$w" height "$h"';' move absolute position ${x}px ${y}px
 }
