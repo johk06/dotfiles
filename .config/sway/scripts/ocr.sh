29c0a8e5939f552719cb7d97f70bfe6b6bc9125f
jhk <jhk06@mailbox.org>
2025-11-02 10:10:00 +0100

    sway: improve ocr script, allow selecting a language

--- .config/sway/scripts/ocr.sh
+++ .config/sway/scripts/ocr.sh
@@ -12,7 +12,7 @@ function notify {
         --action=view="View")"
     case "$response" in
     copy)
-        wl-copy < "$2"
+        wl-copy <"$2"
         ;;
     view)
         xdg-open "$2"
@@ -22,11 +22,20 @@ function notify {
 
 region="$(slurp -w 0 -b '#4c566acc' -s '#ffffff00')"
 sleep 0.1
+
 timestamp="$(date +"$DATEFMT")"
 imagename="$CACHEDIR/$timestamp.png"
+textfile="$CACHEDIR/$timestamp.txt"
+
 if ! grim -g "$region" "$imagename"; then
     exit
 fi
-textfile="$CACHEDIR/$timestamp.txt"
-text="$(tesseract "$imagename" - txt | tee "$textfile" >(wl-copy -p))"
+
+available="$(tesseract --list-langs)"
+if ! language="$(wayinput -l -1 -t "Language" -c $available)"; then
+    exit
+fi
+language="${language:-eng}"
+text="$(tesseract -l "$language" "$imagename" - txt | tee "$textfile" >(wl-copy -p))"
+
 notify "$text" "$textfile"
