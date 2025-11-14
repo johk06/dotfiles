ca66ef3fb933d3dc801eea378b330268f63cb776
jhk <jhk06@mailbox.org>
2025-11-14 07:58:15 +0100

    rofi: fix opening xournalpp files from the file finder

--- .config/rofi/applets/files/files.sh
+++ .config/rofi/applets/files/files.sh
@@ -3,5 +3,15 @@
 if ((ROFI_RETV == 0)); then
     fd -I
 else
-    xdg-open "$1" >/dev/null 2>&1 & disown
+    file="$1"
+    case "$file" in
+    # HACK: Xournalpp uses regular gzip archives, make sure we treat them specially
+    *.xopp)
+        xournalpp "$file" >/dev/null 2>&1 &
+        ;;
+    *)
+        xdg-open "$file" >/dev/null 2>&1 &
+        ;;
+    esac
+    disown
 fi
