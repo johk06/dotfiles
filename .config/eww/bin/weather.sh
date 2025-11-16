d209b469660297fd4bb7535fe499d858328ccda8
jhk <jhk06@mailbox.org>
2025-11-16 16:18:24 +0100

    eww: refactor and improve weather widget
    Add a new weather section to the calendar area, simplify city selection

--- .config/eww/bin/weather.sh
+++ .config/eww/bin/weather.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-CITY="$(eww get weather-city)"
-
-curl -sf "wttr.in/$CITY?format=j1"
+if [ -n "$1" ]; then
+    eww update weather-city="$1" weather="$(curl -rf "wttr.in/$1?format=j1")"
+else
+    CITY="$(eww get weather-city)"
+    curl -sf "wttr.in/$CITY?format=j1"
+fi
