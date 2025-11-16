03d18baf623c6a0f477bcd0329d43f619e6b4619
jhk <jhk06@mailbox.org>
2025-11-16 22:33:18 +0100

    feat(mail): make goimapnotify make sense in the new setup

--- .config/goimapnotify/event.sh
+++ .config/goimapnotify/event.sh
@@ -1,23 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# only show messages received in the last 5 minutes
-RECENT_MESSAGE=300
+ACCOUNT="$1"
+ALIAS="$2"
+REASON="$3"
+EXISTING="$4"
 
-mbsync "$1"
-
-if [[ "$2" == new ]]; then
-    himalaya envelope list -o json order by date desc |
-        jq -r '.[0]| (.date|strptime("%Y-%m-%d %H:%M%z")|mktime) as $time | $time, (.from.name // .from.addr), .subject, ($time | strftime("%H:%M"))' | {
-        curtime=$(date +%s)
-        read -r timestamp
-        read -r author
-        read -r subject
-        read -r date
-        if [ $((curtime - timestamp)) -gt $RECENT_MESSAGE ]; then
-            exit
-        fi
-
-        notify-send -i email "$author" "($date) $subject"
-    }
+if [ ! "$EXISTING" -eq 0 ]; then
+    reply="$(notify-send -i email "$REASON on Account '$ACCOUNT'" "($ALIAS)" \
+        --action=open="Open Aerc")"
+    if [ "$reply" = open ]; then
+        kitty aerc :"change-tab $ACCOUNT"
+    fi
 fi
-notmuch new &
