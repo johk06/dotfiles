b6078deb5623748457dfbe04587b7e258aab3ce4
jhk <jhk06@mailbox.org>
2025-11-17 07:44:48 +0100

    mail: don't store passwords in plaintext in accounts.conf

--- .config/aerc/bootstrap-accounts.sh
+++ .config/aerc/bootstrap-accounts.sh
@@ -6,18 +6,19 @@ if [ -f "$ACCOUNT_CONF" ] && [ "$1" != "reload" ]; then
 fi
 
 pass mail/accounts | while IFS="|" read -r name realname address \
-imapserver smtpserver \
-pass default_folder copy_to; do
-    {
-        read -r user
-        read -r password
-    } < <(pass-get "$pass" user . | jq -Rr @uri)
+    imapserver smtpserver \
+    pass default_folder copy_to; do
+    read -r user < <(pass-get "$pass" user | jq -Rr @uri)
     echo "[$name]"
     echo "from = $realname <$address>"
-    printf "source = $imapserver\n" "$user" "$password"
-    printf "outgoing = $smtpserver\n" "$user" "$password"
+    printf "source = $imapserver\n" "$user"
+    printf 'source-cred-cmd = pass-get "%s" .\n' "$pass"
+    printf "outgoing = $smtpserver\n" "$user"
+    printf 'outgoing-cred-cmd = pass-get "%s" .\n' "$pass"
     echo "default = $default_folder"
-    echo "copy-to = $copy_to"
+    if [[ -n "$copy_to" ]]; then
+        echo "copy-to = $copy_to"
+    fi
     echo
 done >"$ACCOUNT_CONF"
 chmod 600 "$ACCOUNT_CONF"
