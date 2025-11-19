de1d6057d887ecff2a6610a69e192642a6449bcb
jhk <jhk06@mailbox.org>
2025-11-19 22:11:39 +0100

    rofi: greatly improve file applet UX with way better search

--- .config/rofi/applets/files/files.sh
+++ .config/rofi/applets/files/files.sh
@@ -1,17 +1,65 @@
 #!/usr/bin/env bash
 
-if ((ROFI_RETV == 0)); then
-    fd -I
-else
-    file="$1"
-    case "$file" in
+list_files() {
+    fd --no-ignore-vcs $@
+}
+
+open_file() {
+    case "$1" in
     # HACK: Xournalpp uses regular gzip archives, make sure we treat them specially
     *.xopp)
-        xournalpp "$file" >/dev/null 2>&1 &
+        xournalpp "$1" >/dev/null 2>&1 &
         ;;
     *)
-        xdg-open "$file" >/dev/null 2>&1 &
+        xdg-open "$1" >/dev/null 2>&1 &
         ;;
     esac
     disown
+}
+
+parse_flags() {
+    FLAGS="$ROFI_DATA"
+    for flag in "$@"; do
+        case "$flag" in
+        \.*)
+            FLAGS="$FLAGS -e ${flag:1}"
+            ;;
+        =*)
+            FLAGS="$FLAGS -t ${flag:1}"
+            ;;
+        !*)
+            FLAGS="$FLAGS -E ${flag:1}"
+            ;;
+        @*)
+            FLAGS="$FLAGS --changed-within ${flag:1}"
+            ;;
+        '>'*)
+            FLAGS="$FLAGS --changed-after ${flag:1}"
+            ;;
+        '<'*)
+            FLAGS="$FLAGS --changed-before ${flag:1}"
+            ;;
+        *)
+            FLAGS="$FLAGS ${flag}"
+            ;;
+        esac
+    done
+    printf '\0prompt\x1f%s\n' "$FLAGS"
+}
+
+if ((ROFI_RETV == 0)); then
+    printf '\0use-hot-keys\x1ftrue\n'
+    printf '\0prompt\x1f[]\n'
+    list_files ""
+else
+    case "$ROFI_RETV" in
+    1)
+        open_file "$1"
+        ;;
+    2)
+        parse_flags $1
+        printf '\0data\x1f%s\n' "$FLAGS"
+        list_files $FLAGS
+        ;;
+    esac
 fi
