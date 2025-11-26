e5ed8aa0e07503471575761bb08b4345f43f051a
jhk <jhk06@mailbox.org>
2025-11-26 16:43:42 +0100

    sway: improve the way borders look and work sway: add script to toggle borders eww: change b to mean border in config mode

---
+++ .config/sway/scripts/toggle-border.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+BORDER="pixel 2"
+
+border="$(swaymsg -t get_tree | jq -r \
+    '..| ((.nodes? // empty), (.floating_nodes? // empty))[] | select(.focused) |.border? // "none"')"
+
+if [ "$border" = "none" ]; then
+    border="$BORDER"
+else
+    border="none"
+fi
+
+swaymsg '[title=.*]' border "$border" ';' default_border "$border"
