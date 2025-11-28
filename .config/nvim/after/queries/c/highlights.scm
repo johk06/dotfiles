8a83cbe526fafd5a54c22f2941541914f8d8be4d
jhk <jhk06@mailbox.org>
2025-11-28 17:11:28 +0100

    nvim: add some additional (potential) queries for C

--- .config/nvim/after/queries/c/highlights.scm
+++ .config/nvim/after/queries/c/highlights.scm
@@ -7,3 +7,16 @@
     "("
     ")"
   ] @operator)
+
+; highlight #include <some_header.h> like a module import
+(preproc_include
+  path: (_) @module)
+
+; highlight these generic-like type macros in a saner way
+; NOTE: set the priority to override clangd's behavior
+; FIXME: this errors out when included from the c++ queries,
+; since that does not have the (macro_type_specifier) node
+; (macro_type_specifier
+;   name: (_) @constructor
+;   type: (_) @type
+;   (#set! priority 200))
