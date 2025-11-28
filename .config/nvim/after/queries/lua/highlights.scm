84f26b80dfda63b06b5346255c2099fac361a18f
jhk <jhk06@mailbox.org>
2025-11-28 17:26:10 +0100

    nvim/queries: add potential (but still commented out) query for lua require statements

--- .config/nvim/after/queries/lua/highlights.scm
+++ .config/nvim/after/queries/lua/highlights.scm
@@ -9,3 +9,11 @@
   "."
 ] @punctuation.delimiter
   (#set! priority 200))
+
+; NOTE: maybe I want this at some point
+; highlight require calls like module imports
+; (function_call
+;   name: (identifier) @keyword.import
+;   arguments: (arguments
+;     (string) @module)
+;   (#eq? @keyword.import "require"))
