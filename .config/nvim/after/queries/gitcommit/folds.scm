9b780991241b15c9d66f1ee7da2159532ce09777
jhk <jhk06@mailbox.org>
2025-11-28 17:23:34 +0100

    nvim/queries: fold sections in gitcommit

---
+++ .config/nvim/after/queries/gitcommit/folds.scm
@@ -0,0 +1,7 @@
+; extends
+
+; any series of these lines makes a section
+; TODO: figure out a way to include the header line,
+; e.g. "Changes to be committed" in the fold
+((generated_comment
+  (filepath))+) @fold
