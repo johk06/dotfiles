417b25797ce07640054271486a8f87b0584c4115
jhk <jhk06@mailbox.org>
2025-12-08 18:16:40 +0100

    eww: add a clipboard indicator

---
+++ .config/eww/bin/clipboard.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+read -r first_line
+mimetype="$(echo "$first_line" | file --brief --mime-type -)"
+
+case "$mimetype" in
+text/*)
+    text="$first_line"
+    lines=$(wc -l)
+    ;;
+esac
+
+jq -n -c --arg mime "$mimetype" --arg state "$CLIPBOARD_STATE" --arg data "$text" --argjson lines "${lines:-0}" \
+'{data: $data, mime: $mime, state: $state, lines: $lines}' < /dev/null
