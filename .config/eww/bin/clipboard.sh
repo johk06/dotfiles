5d7683b47488973ce18972b439d98bcf9bad718a
jhk <jhk06@mailbox.org>
2025-12-09 19:08:35 +0100

    eww: fix incorrect clipboard mime type detection

--- .config/eww/bin/clipboard.sh
+++ .config/eww/bin/clipboard.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 read -r first_line
-mimetype="$(echo "$first_line" | file --brief --mime-type -)"
+mimetype="$(
+    {
+        echo "$first_line"
+        cat
+    } | file --brief --mime-type -
+)"
 
 case "$mimetype" in
 text/*)
@@ -11,4 +16,4 @@ text/*)
 esac
 
 jq -n -c --arg mime "$mimetype" --arg state "$CLIPBOARD_STATE" --arg data "$text" --argjson lines "${lines:-0}" \
-'{data: $data, mime: $mime, state: $state, lines: $lines}' < /dev/null
+    '{data: $data, mime: $mime, state: $state, lines: $lines}' </dev/null
