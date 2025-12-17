98454e866cfa572f92e705de5784fc2fa6027ceb
jhk <jhk06@mailbox.org>
2025-12-17 16:07:34 +0100

    zsh: simplify aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,32 +1,26 @@
 alias \
-    updategrub="sudo grub-mkconfig -o /boot/grub/grub.cfg" \
-    bc="bc -l" \
-    svim="sudoedit" \
+    g="git" \
     sv="sudoedit" \
     nv="nvim" \
     vi="nvim" \
+    svim="sudoedit" \
     yay="yay --editmenu --devel" \
     q="exit" \
     rm="rm -i" \
-    ll='lsd -l --hyperlink=auto' \
-    lls='lsd -l --blocks=size,name --total-size --sort=size' \
-    lla='lsd -lA --hyperlink=auto' \
-    llv='lsd -l --hyperlink=auto --blocks=group,user,permission,git,date,size,links,name'\
-    la='lsd -A --hyperlink=auto' \
     l='lsd --hyperlink=auto' \
-    grep='grep --color=auto' \
-    g="git" \
+    ll='lsd -l --hyperlink=auto' \
+    la='lsd -A --hyperlink=auto' \
+    lla='lsd -lA --hyperlink=auto' \
+    lls='lsd -l --blocks=size,name --total-size --sort=size' \
+    llv='lsd -l --hyperlink=auto --blocks=group,user,permission,git,date,size,links,name' \
+    grep='grep --color=auto'
 
 alias '#'="noglob qalc" # do math directly on the cmdline
 
 # nice to have redirections
 alias \
     -g "@quiet"=">/dev/null 2>&1" \
-    -g "@e2o"="2>&1" \
-    -g "@o2e"=">&2" \
-    -g '@noe'="2>/dev/null" \
-    -g "@noo"=">/dev/null" \
-    -g "@help"='--help 2>&1 | bat -l help -p' \
+    -g "@help"='--help 2>&1 | bat -l help -p'
 
 TAB=$'\t'
 GH="https://github/com"
