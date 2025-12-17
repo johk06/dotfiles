f622f0bfcf848fbfd9bdb932bcbda92c84f74361
jhk <jhk06@mailbox.org>
2025-12-17 18:18:52 +0100

    eww: update clipboard

--- .config/eww/bin/clipboard.sh
+++ .config/eww/bin/clipboard.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-read -r first_line
-mimetype="$(
-    {
-        echo "$first_line"
-        cat
-    } | file --brief --mime-type -
-)"
+mimetype="$(wl-paste -l | sed 1q)"
 
 case "$mimetype" in
 text/*)
+    read -r first_line
     text="$first_line"
     lines=$(wc -l)
     ;;
 esac
 
 jq -n -c --arg mime "$mimetype" --arg state "$CLIPBOARD_STATE" --arg data "$text" --argjson lines "${lines:-0}" \
-    '{data: $data, mime: $mime, state: $state, lines: $lines}' </dev/null
+    '{data: $data|@json, mime: $mime, state: $state, lines: $lines}' </dev/null
