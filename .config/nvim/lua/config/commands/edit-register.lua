9da0cb66e58da7be7d0fa5d029f9c84825c81d17
jhk <jhk06@mailbox.org>
2025-12-18 13:30:55 +0100

    nvim: use new and improved vim.ui.input to edit registers
    This is now much nicer with the full-window view introduced in ca59836b
    Now cq also ignores spaces and reproduces ' ' as <Space> as well

--- .config/nvim/lua/config/commands/edit-register.lua
+++ .config/nvim/lua/config/commands/edit-register.lua
@@ -1,54 +1,28 @@
 local M = {}
 
-local api = vim.api
 local fn = vim.fn
 local utils = require("config.utils")
 
 local set_from_str = function(s, reg)
-    fn.setreg(reg, vim.keycode(s))
+    local text = s:gsub("%s", "")
+    fn.setreg(reg, vim.keycode(text))
 end
 
 local get_macro_str = function(reg)
     local content = fn.getreg(reg, 1, true) ---@cast content string[] with list=true
-    return fn.keytrans(table.concat(content, "\n")):gsub("<Space>", " ")
+    return fn.keytrans(table.concat(content, "\n"))
 end
 
 M.MACRO_SAVEPATH = fn.stdpath("config") .. "/saved-macros.json"
 
 ---@param reg string
 M.edit_macro = function(reg)
-    local buf = api.nvim_create_buf(false, true)
     local keys = get_macro_str(reg)
-    api.nvim_buf_set_lines(buf, 0, -1, false, { keys })
-
-    api.nvim_buf_set_name(buf, ("[Register: @%s]"):format(reg))
-
-    local win = utils.win_show_buf(buf, {
-        position = "float",
-        title = "Editing macro @" .. reg
-    })
-
-    local bo = vim.bo[buf]
-    bo.buftype = "acwrite"
-    bo.modified = false
-    local augroup
-    augroup = utils.autogroup("config.edit-macro." .. reg, {
-        BufWriteCmd = {
-            buffer = buf,
-            callback = function()
-                local lines = api.nvim_buf_get_lines(buf, 0, -1, false)
-                set_from_str(table.concat(lines, ""))
-                bo.modified = false
-            end
-        },
-        BufLeave = {
-            buffer = buf,
-            callback = function()
-                api.nvim_buf_delete(buf, { force = true })
-                api.nvim_del_augroup_by_id(augroup)
-            end
-        }
-    })
+    vim.ui.input({ prompt = ("Edit @%s"):format(reg), default = keys}, function(new)
+        if new then
+            set_from_str(new, reg)
+        end
+    end)
 end
 
 local macro_cache = {}
