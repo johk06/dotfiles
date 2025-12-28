b865f69c49375a6d4b26e10286e8f85bd44e4d73
jhk <jhk06@mailbox.org>
2025-12-28 14:56:11 +0100

    nvim: improve additions to gitcommit ftplugin

--- .config/nvim/after/ftplugin/gitcommit.lua
+++ .config/nvim/after/ftplugin/gitcommit.lua
@@ -1,2 +1,5 @@
-vim.wo[0][0].spell = true
-vim.cmd.startinsert()
+local firstline = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
+if firstline:match("^%s*$") then
+    local root = require("config.lib.fs").get_project_root()
+    vim.snippet.expand(("${1:%s}: $0"):format(vim.fs.basename(root)))
+end
