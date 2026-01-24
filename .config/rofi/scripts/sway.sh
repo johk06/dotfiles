ffb780bc862be2be7c6c850c0f7a6ac281ed9f03
jhk <jhk06@mailbox.org>
2026-01-24 12:48:53 +0100

    rofi: improve sway window applet

--- .config/rofi/scripts/sway.sh
+++ .config/rofi/scripts/sway.sh
@@ -4,18 +4,16 @@ ICON="\0icon\x1f"
 INFO="\x1finfo\x1f"
 
 read -r -d '' JQ_QUERY <<'JQ'
-.. | ((.nodes? // empty)
-    + (.floating_nodes? // empty))[] 
-| select(.pid)
-| "\(.id)\t\(.name)\t\(.app_id? // .window_properties.class)\t\(if .focused then 1 else 0 end)"
+.nodes.[].nodes[] |
+.name as $name |..|
+((.nodes? // empty) + (.floating_nodes? // empty))[] |
+select(.pid) + {ws: $name}
+| "\(.id)\t\(.name)\t\(.app_id? // .window_properties.class)\t\(.ws)"
 JQ
 
 if ((ROFI_RETV == 0)); then
     echo -en "\0use-hot-keys\x1ftrue\n\0delim\x1f\t\n"
-    while IFS=$'\t' read -r id name class is_active; do
-        if ((is_active)); then
-            continue
-        fi
+    while IFS=$'\t' read -r id name class ws; do
 
         icon="$class"
         if [[ "$class" == "kitty" ]]; then
@@ -25,9 +23,15 @@ if ((ROFI_RETV == 0)); then
             ncmpcpp:*) icon="multimedia-audio-player" ;;
             qalc) icon="qalculator" ;;
             aerc) icon=email ;;
+            newsboat) icon=$name ;;
             esac
         fi
-        printf "%s\n%s$ICON%s$INFO%s\t" "$name" "$class" "$icon" "$id"
+
+        case "$ws" in
+        __i3_scratch) workspace="~scratch" ;;
+        *) workspace="$ws" ;;
+        esac
+        printf "%s\n%s$ICON%s$INFO%s\t" "$name" "$workspace - $class" "$icon" "$id"
     done < <(swaymsg -t get_tree | jq -r "$JQ_QUERY")
 else
     if ((ROFI_RETV == 10)); then
