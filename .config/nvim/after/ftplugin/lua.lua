fa731327286c11ab48afc26a0c879f6840b07225
jhk <jhk06@mailbox.org>
2026-03-29 16:54:09 +0200

    nvim: make \f macro for lua more robust

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,6 +1,6 @@
 local map = require("config.utils").ft_mapper()
 
-map("n", "<localleader>f", [[ysa")f)a:format()<Left>]], {
+map("n", "<localleader>f", [[ysa")%a:format()<Left>]], {
     remap = true,
     desc = "Lua: Format string"
 })
