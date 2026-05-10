b3f7e3b6c4e79c03aa08341b840a4678daa3faf0
jhk <jhk06@mailbox.org>
2026-05-10 16:02:25 +0200

    nvim: add notification to cut-out

--- .config/nvim/lua/plugins/cut-out.lua
+++ .config/nvim/lua/plugins/cut-out.lua
@@ -5,6 +5,9 @@ return {
     ---@diagnostic disable-next-line: missing-fields
     opts = {
         -- more ergonomic to hit, works well with the yanking nature of the plugin
-        keys = "yd"
+        keys = "yd",
+        on_found = function(matches, _)
+            require("config.utils").message("Cut-Out", ("%d matches"):format(#matches))
+        end
     }
 }
