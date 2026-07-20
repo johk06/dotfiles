7364e5efd8086926ba4e269320937e5572ad5a81
jhk <jhk06@mailbox.org>
2026-07-20 08:47:31 +0200

    nvim: improve lua snippets

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -12,6 +12,14 @@ map("n", "<localleader>p", function()
     vim.cmd.normal { "ipcall(", bang = true }
 end, { desc = "Lua: Protect Call" })
 
+local loop_snippet = function(vars, iterator)
+    return {
+        ("for %s in %s do"):format(vars, iterator),
+        "\t$0",
+        "end"
+    }
+end
+
 local abbrevs = {
     ll = "local $1 = $0",
     lt = "$1 = $0",
@@ -20,6 +28,11 @@ local abbrevs = {
     ff = { "function$1($2)", "\t$0", "end" },
     fl = { "function($1)", "\t$0", "end" },
     tc = { 'if type($1) == "$2" then', "\t$0", "end" },
+    -- for {list,table} {values,pairs}
+    Flv = loop_snippet("_, ${1:v}", "ipairs($2)"),
+    Flp = loop_snippet("${1:i}, ${2:v}", "ipairs($3)"),
+    Ftv = loop_snippet("_, ${1:v}", "pairs($2)"),
+    Ftp = loop_snippet("${1:k}, ${2:v}", "pairs($3)"),
 }
 
 for abbr, fn in pairs(abbrevs) do
