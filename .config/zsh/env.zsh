c3c3063c3f5b0a4daa0a3c135cde3eaabe127a3b
jhk <jhk06@mailbox.org>
2026-09-04 15:01:30 +0200

    zsh: make the restic system system agnostic

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,11 +1,9 @@
-if [[ "$HOST" == "hpc" ]]; then
-    if [[ ! -f "$ZCACHEDIR/env.zsh" ]]; then
-        {
-            local cloud_backend="$(pass-get backup/pc backend)"
-            local url="$(pass-get "$cloud_backend" backup)"
-            printf 'export RESTIC_REPOSITORY=%q\n' "$url"
-            printf 'export RESTIC_PASSWORD_COMMAND="pass-get backup/pc ."\n'
-        } > "$ZCACHEDIR/env.zsh"
-    fi
-    source "$ZCACHEDIR/env.zsh"
+if [[ ! -f "$ZCACHEDIR/env.zsh" ]]; then
+    {
+        local cloud_backend="$(pass-get backup/main backend)"
+        local url="$(pass-get "$cloud_backend" backup)"
+        printf 'export RESTIC_REPOSITORY=%q:%q\n' "$url" "$HOST"
+        printf 'export RESTIC_PASSWORD_COMMAND="pass-get backup/main ."\n'
+    } > "$ZCACHEDIR/env.zsh"
 fi
+source "$ZCACHEDIR/env.zsh"
