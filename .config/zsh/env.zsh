b51ff4b395c5cc147f32787efafbd90d7e00ae41
jhk <jhk06@mailbox.org>
2026-09-04 15:06:38 +0200

    zsh: make the restic password host dependant

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,9 +1,9 @@
 if [[ ! -f "$ZCACHEDIR/env.zsh" ]]; then
     {
-        local cloud_backend="$(pass-get backup/main backend)"
+        local cloud_backend="$(pass-get backup/$HOST backend)"
         local url="$(pass-get "$cloud_backend" backup)"
         printf 'export RESTIC_REPOSITORY=%q:%q\n' "$url" "$HOST"
-        printf 'export RESTIC_PASSWORD_COMMAND="pass-get backup/main ."\n'
+        printf 'export RESTIC_PASSWORD_COMMAND="pass-get backup/%s ."\n' "$HOST"
     } > "$ZCACHEDIR/env.zsh"
 fi
 source "$ZCACHEDIR/env.zsh"
